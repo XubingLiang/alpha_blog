c65e910dd95ee42af8500116e43acdfbd7f084fb
Ryan <liangxubing@gmail.com>
2016-04-14 17:36:04 +1000

    add authenticate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
 
 	def create
-		debugger
+
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
 		@article.user = User.first
